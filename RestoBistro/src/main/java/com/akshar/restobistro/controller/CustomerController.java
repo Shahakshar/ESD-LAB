e2151b8735428ae16aafa21d2ed70bd3cb7a98fa
aksharshah <akshau0123@gmail.com>
2024-11-12 00:36:53 +0530

    Customer Update endpoint implemented.

--- RestoBistro/src/main/java/com/akshar/restobistro/controller/CustomerController.java
+++ RestoBistro/src/main/java/com/akshar/restobistro/controller/CustomerController.java
@@ -5,9 +5,7 @@ import com.akshar.restobistro.services.CustomerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,21 @@ public class CustomerController {
         User currentUser = (User) authentication.getPrincipal();
 
         return ResponseEntity.ok(currentUser);
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<String> updateUser(@RequestBody User updateUser) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        String email = currentUser.getEmail();
+
+        boolean updated = customerService.updateUser(email, updateUser);
+
+        if (updated) {
+            return ResponseEntity.ok("User updated successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- RestoBistro/src/main/java/com/akshar/restobistro/services/CustomerService.java
+++ RestoBistro/src/main/java/com/akshar/restobistro/services/CustomerService.java
@@ -4,8 +4,11 @@ import com.akshar.restobistro.models.User;
 import com.akshar.restobistro.repo.UserRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -14,6 +17,22 @@ public class CustomerService {
 
     public CustomerService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public boolean updateUser(String email, User updateUser) {
+        Optional<User> userExists = userRepository.findByEmail(email);
+//        System.out.println(updateUser + "\n\n" + userExists);
+        if (userExists.isPresent()) {
+            System.out.println(updateUser + "\n\n" + userExists);
+            User user = userExists.get();
+            user.setFullName(updateUser.getFullName());
+            user.setEmail(updateUser.getEmail());
+            user.setUpdatedAt(new Date());
+            System.out.println(user);
+            userRepository.save(user);
+            return true;
+        }
+        return false;
     }
 
 }
